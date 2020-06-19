7dbd51248426ced97278e768ac55291796c44c74
kang naeon <64356881+kang0202@users.noreply.github.com>
2020-06-19 10:17:34 +0900

    commit

---
+++ RazorPagesMovie_kang/Data/RazorPagesMovie_kangContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie_kang.Models;
+
+namespace RazorPagesMovie_kang.Data
+{
+    public class RazorPagesMovie_kangContext : DbContext
+    {
+        public RazorPagesMovie_kangContext (DbContextOptions<RazorPagesMovie_kangContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie_kang.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie_kang/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie_kang.Data;
+using RazorPagesMovie_kang.Models;
+
+namespace RazorPagesMovie_kang.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie_kang.Data.RazorPagesMovie_kangContext _context;
+
+        public IndexModel(RazorPagesMovie_kang.Data.RazorPagesMovie_kangContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
